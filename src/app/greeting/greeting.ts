29435e7836be33a3868e067ef6f48849ccaf3c63
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2015-12-16 13:44:49 +0000

    Renamed component => app

--- src/app/greeting/greeting.ts
+++ src/app/greeting/greeting.ts
@@ -2,8 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'greeting',
-  templateUrl: 'component/greeting/greeting.html',
-  styleUrls: ['component/greeting/greeting.css']
+  templateUrl: 'app/greeting/greeting.html',
+  styleUrls: ['app/greeting/greeting.css']
 })
 export default class Greeting {
   greeting = 'Hello World';
